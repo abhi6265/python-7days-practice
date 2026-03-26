5e1efe6f135a834ce19f1f18558ce835cb1ff255
Abhi6265 <abhi6265kaushik@gmail.com>
2026-03-27 00:40:12 +0530

    Day 1: Basics, variables, input/output completed

--- Day1/practice.py
+++ Day1/practice.py
@@ -5,3 +5,15 @@ b=int(input())
 print("Addition",a+b)
 print("Substraction",a-b)
 print("Multiplication",a*b)
+
+#Swapping of two number without using third variable
+a=10 
+b=20
+
+a=a+b
+b=a-b
+a=a-b
+print("After swapping a:",a)
+print("After swapping b:",b)
+
+
