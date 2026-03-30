d75c54ae909f004a44bcbc57852b3c835b361b8a
Abhi6265 <abhi6265kaushik@gmail.com>
2026-03-30 23:14:37 +0530

    day3 practice

---
+++ Day3/practice.py
@@ -0,0 +1,14 @@
+# print counting 1-100
+for i in range(1,101):
+    print(i)
+    
+
+#print all even number between 1-100
+for i in range(1,101):
+    if i%2==0:
+        print(i)
+        
+#print odd number between 1-50
+for i in range(1,51,2):
+    print(i)
+    
