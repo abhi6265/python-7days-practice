e4ec2682f5a46a9ec6e199304f06f5e4888f8765
Abhi6265 <abhi6265kaushik@gmail.com>
2026-04-09 00:49:01 +0530

    practice

---
+++ Day4/List.py
@@ -0,0 +1,19 @@
+#list store multiple value in one variable.
+num=[1,2,3,4,5]
+# accessing and Indexing
+print(num[0])
+print(num[-1])
+
+#sclicing
+print(num[1:3])
+
+#modify list 
+num1=[23,45,4,3,5,3]
+num1[0]=9
+print(num1)
+
+# add elements (Append)
+
+num2=[11,12,13,14,15]
+num2.append(69) #add at end
+num2.appen(3,36)#add at index
