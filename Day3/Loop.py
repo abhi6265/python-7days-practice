58f733155fc5659ec504b6977c706a18cf51e75b
Abhi6265 <abhi6265kaushik@gmail.com>
2026-04-09 01:41:09 +0530

    For loop, while loop

--- Day3/Loop.py
+++ Day3/Loop.py
@@ -1,6 +1,34 @@
 # Loop
 # Make your code repeat tasks automatically and build logic strength
+#used when you know how many you need to run the loop
+# for i in range(5):
+#     print(i)
 
-for i in range(5):
-    print(i)
+# used when you dont know how many time you need to run the code
+
+# i=0
+# while i<5:
+#     print(i ,end=" ")
+#     i+=1
     
+# #Break used to stop the loop
+# for i in range(5):
+#     if i==4:
+#         break
+#     print(i,end=(""))
+
+
+# continue use to skip to curren iteration and move to next
+
+# i=0
+# while i<5:
+#     i+=1
+#     if i==3:
+#         continue
+#     print(i)
+    
+#nested loop 
+for i in range(5):
+    for j in range(5):
+        print(i,j)
+
