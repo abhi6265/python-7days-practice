cddb7967f1f85e05aa1f7167ef9367b34f0cf751
Abhi6265 <abhi6265kaushik@gmail.com>
2026-04-13 23:50:23 +0530

    String_Function

---
+++ Day5/String_Function.py
@@ -0,0 +1,21 @@
+# Strings:-Text data
+Text="String"
+
+# String Common method
+print(Text.upper())
+print(Text.lower())
+print(len(Text))
+
+#indexing
+print(Text[1]) #t
+print(Text[0]) #S
+# Slicing
+print(Text[1:3])
+print(Text[2:])
+
+# Reversing String
+print(Text[::-1])
+
+
+
+
